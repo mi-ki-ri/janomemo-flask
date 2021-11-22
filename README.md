980140bf9429e66260169f6acbf798e68b8f9b6e
HOTTA Tatsuya <hotta.tatsuya.fcc@gmail.com>
2021-11-22 11:53:05 +0900

    user_dic

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 1. `flask run`
 1. フォームに入力して送信
 1. キーワード列を得る
+
+
+```sh
+nkf -w jawiki-latest-all-titles-in-ns0 | grep -v '[!-/:-@≠\[-`{-~]' | grep -v '^[0-9]' | egrep -v '^[ぁ-ん]{2}' | grep -v 'の一覧' | grep -v '曖昧さ回避' | awk '{ print length($1),$1; }' | grep -v '1[[:space:]]' | sed -r 's/.*[[:space:]](.*)/\1,名詞,\1/g' > user_dic.csv
+
+```
--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from janome.charfilter import UnicodeNormalizeCharFilter
 
 from janome.tokenizer import Tokenizer
-
+from janome.analyzer import Analyzer
+from janome.tokenfilter import CompoundNounFilter, POSKeepFilter
 
 app = Flask(__name__)
+t = Tokenizer("user_dic.csv", udic_type="simpledic", udic_enc="utf8")
 
 
 @app.route('/')
@@ -15,17 +18,25 @@ def index():
 
 @app.route('/post', methods=['POST'])
 def hello():
-    t = Tokenizer()
     post_text = request.form['text']
     tags = []
-    for token in t.tokenize(f"{post_text}"):
-      print(token)
-      if token.part_of_speech.split(',')[0] == "名詞" and (token.part_of_speech.split(",")[1] == "一般" or token.part_of_speech.split(",")[1] == "固有名詞" ):
-        tags.append(token.surface)
+    syurui="名詞"
+
+    cf = [UnicodeNormalizeCharFilter()]#前処理フィルタ
+    tf = [CompoundNounFilter(),POSKeepFilter([syurui])]
+    a = Analyzer(char_filters=cf,token_filters=tf)#解析器生成
+
+    for token in a.analyze(post_text):
+        print(token)
+        if token.part_of_speech.split(',')[1] == "固有名詞" or token.part_of_speech.split(',')[1] == "一般":
+            tags.append(token.surface)
+
+        
 
     ret_text = ""
     for tag in list(set(tags)):
-      ret_text += f'"{tag}",'
+        if len(tags) >= 2:
+          ret_text += f'"{tag}",'
 
     ret_text = f"keywords: [{ret_text[:-1]}]"
     return ret_text
